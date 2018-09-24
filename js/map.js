bc4557eb17fcd054271430a3275dc4fb2b58fdf3
lowsisad <lowsisad@gmail.com>
2018-09-25 00:34:51 +0300

    Вне изменения

--- js/map.js
+++ js/map.js
@@ -2,19 +2,49 @@
 
 var numbersOfOffers = 8;
 var width = 600;
-var avatars = [1, 2, 3, 4, 5, 6, 7, 8];
-var titles = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
-var types = {'palace': 'дворец', 'flat': 'квартира', 'house': 'дом', 'bungalo': 'бунгало'};
-var checkins = ['12:00', '13:00', '14:00'];
-var checkouts = ['12:00', '13:00', '14:00'];
-var someFeatures = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-var somePhotos = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+
+var maxCost = 1000000;
+var minCost = 1000;
+var maxLocalY = 630;
+var minLocalY = 130;
+var maxRooms = 4;
+var minRooms = 1;
+var maxGuests = 10;
+
+var TITLES = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
+var TYPES = {'palace': 'дворец',
+ 'flat': 'квартира',
+ 'house': 'дом',
+ 'bungalo': 'бунгало'
+};
+var CHECKINS = ['12:00', '13:00', '14:00'];
+var CHECKOUTS = ['12:00', '13:00', '14:00'];
+var SOMEFEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var SOMEPHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 
 var getElement = function (someElement) {
   var random = someElement[Math.floor(Math.random() * (someElement.length))];
   return random;
 };
 
+function getShuffle(arr){
+	var j, temp;
+	for(var i = arr.length - 1; i > 0; i--){
+		j = Math.floor(Math.random()*(i + 1));
+		temp = arr[j];
+		arr[j] = arr[i];
+		arr[i] = temp;
+	}
+	return arr;
+}
+
+var getAvatars = function (){
+  var r = [];
+  for (var i = 1; i <= 8; i++) r.push(i);
+  return r;
+}
+
+var avatars = getAvatars();
 
 var getFewElements = function (array) {
   var number = Math.floor(Math.random() * (array.length - 1) + 1);
@@ -25,40 +55,34 @@ var getFewElements = function (array) {
   return staff;
 };
 
-
-var getRandom = function compareRandom() {
-  return Math.random() - 0.5;
-};
-
-var offers = [];
-
 var getOffers = function () {
+  var offers = [];
   for (var i = 0; i < numbersOfOffers; i++) {
-    avatars.sort(getRandom);
-    titles.sort(getRandom);
-    somePhotos.sort(getRandom);
+    avatars = getShuffle(avatars);
+    TITLES = getShuffle(TITLES);
+    SOMEPHOTOS = getShuffle(SOMEPHOTOS);
     var localX = Math.floor(Math.random() * (width));
-    var localY = Math.floor(Math.random() * (630 - 130) + 130);
-    var Price = Math.floor(Math.random() * (1000000 - 1000) + 1000);
-    var roomsNumber = Math.floor(Math.random() * (4 - 1) + 1);
-    var guestsNumber = Math.floor(Math.random() * (10));
-    var getType = types[Object.keys(types)[Math.floor(Math.random() * (Object.keys(types).length))]];
+    var localY = Math.floor(Math.random() * (maxLocalY - minLocalY) + minLocalY);
+    var Price = Math.floor(Math.random() * (maxCost - minCost) + minCost);
+    var roomsNumber = Math.floor(Math.random() * (maxRooms - minRooms) + minRooms);
+    var guestsNumber = Math.floor(Math.random() * (maxGuests));
+    var getType = TYPES[Object.keys(TYPES)[Math.floor(Math.random() * (Object.keys(TYPES).length))]];
     var array = {
       'author': {
         'avatar': 'img/avatars/user0' + avatars[i] + '.png'
       },
       'offer': {
-        'title': titles[i],
+        'title': TITLES[i],
         'address': localX + ',' + localY,
         'price': Price,
         'type': getType,
         'rooms': roomsNumber,
         'guests': guestsNumber,
-        'checkin': getElement(checkins),
-        'checkout': getElement(checkouts),
-        'features': getFewElements(someFeatures),
+        'checkin': getElement(CHECKINS),
+        'checkout': getElement(CHECKOUTS),
+        'features': getFewElements(SOMEFEATURES),
         'description': '',
-        'photos': [somePhotos[0], somePhotos[1], somePhotos[2]],
+        'photos': [SOMEPHOTOS[0], SOMEPHOTOS[1], SOMEPHOTOS[2]],
       },
       'location': {
         'x': localX,
@@ -67,9 +91,9 @@ var getOffers = function () {
     };
     offers.push(array);
   }
+  return offers;
 };
-
-getOffers();
+var offers = getOffers();
 
 var mapPinsListElement = document.querySelector('.map__pins');
 var templatePin = document.querySelector('#pin');
@@ -90,18 +114,15 @@ var renderOffer = function (protoPin) {
   offerAtt.querySelector('.popup__text--time').innerHTML = 'Заезд после ' + protoPin.offer.checkin + ', выезд до ' + protoPin.offer.checkout;
   offerAtt.querySelector('.popup__features').createElement = protoPin.offer.features;
   offerAtt.querySelector('.popup__description').innerHTML = protoPin.offer.description;
-  var someFragment = document.createDocumentFragment();
-
   for (var m = 0; m < 3; m++) {
     var img = document.createElement('img');
     img.src = protoPin.offer.photos[m];
     img.width = '45';
     img.height = '40';
+    img.classList.add('popup__photo');
     img.alt = 'Фотография жилья';
-    someFragment.appendChild(img);
+    offerAtt.querySelector('.popup__photos').appendChild(img);
   }
-
-  offerAtt.querySelector('.popup__photos').appendChild(someFragment);
   offerAtt.querySelector('.popup__avatar').innerHTML = protoPin.author.avatar;
   offerAtt.querySelector('.popup__photos img:first-child').remove();
   return offerAtt;
@@ -118,19 +139,15 @@ var renderPin = function (protoPin) {
   return pinAtt;
 };
 
-var fragment = document.createDocumentFragment();
-
+var fragmentWithPins = document.createDocumentFragment();
+var fragmentWithOffers = document.createDocumentFragment();
 for (var i = 0; i < offers.length; i++) {
 
-  fragment.appendChild(renderPin(offers[i]));
+  fragmentWithPins.appendChild(renderPin(offers[i]));
+  fragmentWithOffers.appendChild(renderOffer(offers[i]));
 }
-mapPinsListElement.appendChild(fragment);
-
-for (i = 0; i < offers.length; i++) {
-
-  fragment.appendChild(renderOffer(offers[i]));
-}
-mapOffersListElement.appendChild(fragment);
+mapPinsListElement.appendChild(fragmentWithPins);
+mapOffersListElement.appendChild(fragmentWithOffers);
 
 var userDialog = document.querySelector('.map');
 userDialog.classList.remove('map--faded');
